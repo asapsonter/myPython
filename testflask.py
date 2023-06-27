b8bc062abadea084fd76a9fc7258ba48a822f505
Sam <aarga@sycratech.com>
2023-06-27 18:09:24 +0800

    mysql db first table

--- testflask.py
+++ testflask.py
@@ -2,15 +2,32 @@ import html
 from flask import Flask,  render_template, request, escape
 from vsearch import searchForletters
 
+
 app = Flask (__name__)
 
  
 
+import mysql.connector
 def log_request(req: 'flask_request', res: str) -> None:
-    with open('vsearch.log', 'a') as log: #open a log file called vsearch_log
-       #print(str(dir(req)), res, file=log) # print vals req $ res in file display as log 
-        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
-       
+    """Log details of the web request and the results."""
+    dbconfig = { 'host': '127.0.0.1',
+                 'user': 'vsearch',
+                 'password': 'vsearchpasswd',
+                 'database': 'vsearchlogDB', }
+    conn = mysql.connector.connect(**dbconfig)
+    cursor = conn.cursor()
+    _SQL = """insert into log
+              (phrase, letters, ip, browser_string, results)
+              values
+              (%s, %s, %s, %s, %s)"""
+    cursor.execute(_SQL, (req.form['phrase'],
+                          req.form['letters'],
+                          req.remote_addr,
+                          req.user_agent.browser,
+                          res, ))
+    conn.commit()
+    cursor.close()
+    conn.close()
 
 @app.route('/search4', methods=['POST']) #decorator identify function as POST
 def do_search() -> 'html':
